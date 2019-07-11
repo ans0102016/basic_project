2083e3ed9d527a750976db632d317c1313f656af
Ameet Swamy <ameet79@gmail.com>
2019-07-11 13:59:41 +0530

    http observables

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -31,7 +31,9 @@ export class PassengerDashboardComponent implements OnInit {
     constructor(private passengerService: PassengerDashboardService) {
     }
     ngOnInit() {
-      this.passengers = this.passengerService.getPassengers();
+      this.passengerService
+        .getPassengers()
+        .subscribe((data: Passenger[]) => this.passengers = data);
     }
     handleEdit(event: Passenger) {
       this.passengers = this.passengers.map((passenger: Passenger)=>{
